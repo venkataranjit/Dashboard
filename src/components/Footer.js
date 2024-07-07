40ffc4fc9930c047f23dfff6d09c94bd36a3cedf
venkataranjit <112625318+venkataranjit@users.noreply.github.com>
2024-07-07 18:14:49 +0530

    Routers Updated

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+let Footer = () => {
+  return (
+    <>
+      <footer className="footer">
+        <div className="container-fluid">
+          <span className="">This is a Sticky Footer.</span>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
