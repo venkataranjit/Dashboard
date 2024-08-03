11c9ad7fc5e6c25c76579a2ff09f7555930fb813
venkataranjit <112625318+venkataranjit@users.noreply.github.com>
2024-08-03 14:59:59 +0530

    search filter added in todo

--- src/pages/Integrations.js
+++ src/pages/Integrations.js
@@ -2,40 +2,81 @@ import React, { useState } from "react";
 
 let Integrations = () => {
   const [input, setInput] = useState("");
+  const [edit, setEdit] = useState("");
   const [error, setError] = useState(null);
+  const [search, setSearch] = useState("");
 
   const [todo, setTodo] = useState([
-    { id: 1, value: "Dummy" },
-    { id: 2, value: "Dummy 2" },
+    { todoList: "This is First Todo", id: 1 },
+    { todoList: "This is Second Todo", id: 2 },
   ]);
+  const inputHandler = (e) => {
+    setInput(e.target.value);
+  };
 
-  const errorFunction = (msg, type) => {
-    setError({
-      msg: msg,
-      type: type,
-    });
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (input.length === 0) {
+      errorHandler("Add Todo to Submit", "danger");
+    } else {
+      if (edit !== null) {
+        setTodo((prevTodo) =>
+          prevTodo.map((item) =>
+            item.id === edit ? { ...item, todoList: input } : item
+          )
+        );
+        setEdit(null);
+        errorHandler("Todo Updated Successfully", "success");
+      } else {
+        {
+          setTodo((prevTodo) => [
+            { todoList: input, id: Date.now() },
+            ...prevTodo,
+          ]);
+          errorHandler("Todo Added Successfully", "success");
+        }
+      }
+      setInput("");
+    }
+  };
+
+  const deleteHandler = (id) => {
+    setTodo((prevtodo) => prevtodo.filter((item) => item.id !== id));
+    errorHandler("Todo Deleted Successfully", "success");
+  };
+
+  const editHandler = (id) => {
+    // const itemToEdit = todo.find((item) => item.id === id);
+    // setInput(itemToEdit.todoList);
+    setInput(todo.find((item) => item.id === id).todoList);
+    setEdit(id);
+  };
+
+  const errorHandler = (msg, type) => {
+    setError({ message: msg, type: type });
     setTimeout(() => {
       setError(null);
     }, 2000);
   };
 
-  const inputHandler = (e) => {
-    setInput(e.target.value);
-  };
-  const clickHandler = (e) => {
-    e.preventDefault();
-    if (input === "") {
-      errorFunction("Please Add Any Task", "danger");
+  const searchHandler = (e) => {
+    setSearch(e.target.value);
+
+    const filteredTodo = todo.filter((item) =>
+      item.todoList.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+
+    if (filteredTodo.length === 0) {
+      errorHandler("No Todo Matched", "danger");
     } else {
-      setTodo((prevTodo) => [{ id: Date.now(), value: input }, ...prevTodo]);
-      setInput("");
-      errorFunction("Task Added Successfully", "success");
+      setError(null);
     }
   };
-  const deleteHandler = (id) => {
-    setTodo((prevTodo) => prevTodo.filter((item) => item.id !== id));
-    errorFunction("Deleted Successfully", "success");
-  };
+
+  const filteredTodo = todo.filter((item) =>
+    item.todoList.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <>
       <main
@@ -46,44 +87,59 @@ let Integrations = () => {
         <h2>Todo List</h2>
         <div className="jumbotron">
           <div className="container">
+            <input
+              className="form-control mb-3 col-sm-2 float-right"
+              name="search"
+              value={search}
+              placeholder="search"
+              onChange={searchHandler}
+            />
             <div className="input-group mb-3">
               <input
                 type="text"
-                value={input}
-                name="input"
-                onChange={inputHandler}
                 className="form-control"
+                name="input"
+                value={input}
+                onChange={inputHandler}
               />
               <button
                 className="btn btn-secondary"
                 type="button"
-                onClick={clickHandler}
+                onClick={submitHandler}
               >
-                Submit
+                {edit !== null ? "Update" : "Submit"}
               </button>
             </div>
             {error && (
               <div className={`alert alert-${error.type} mt-3`} role="alert">
-                {error.msg}
+                {error.message}
               </div>
             )}
+
             <ul className="list-group">
-              {todo.map((item, index) => {
-                return (
-                  <li
-                    key={item.id}
-                    className="list-group-item d-flex justify-content-between align-items-center"
-                  >
-                    {item.value}
+              {filteredTodo.map((item) => (
+                <li
+                  key={item.id}
+                  className="list-group-item d-flex justify-content-between align-items-center"
+                >
+                  {item.todoList}
+                  <span>
                     <span
-                      onClick={() => deleteHandler(item.id)}
                       className="badge bg-primary rounded-pill text-white"
+                      onClick={() => editHandler(item.id)}
+                    >
+                      Edit
+                    </span>
+                    <span
+                      style={{ marginLeft: "5px" }}
+                      className="badge bg-primary rounded-pill text-white"
+                      onClick={() => deleteHandler(item.id)}
                     >
                       X
                     </span>
-                  </li>
-                );
-              })}
+                  </span>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
